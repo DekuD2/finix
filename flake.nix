d11a7c7e1c65e4b8a65758db0a1e9376d5283d6c
Filip R. <deku@outlook.cz>
2025-04-10 16:52:48 +0200

    changed to shell

--- flake.nix
+++ flake.nix
@@ -9,28 +9,25 @@
   in
   {
     lib.${system} = {
-      mk_prepare_venv = {writeScriptBin, python-with-packages, venvDir ? ".venv"}: (writeScriptBin "prepare_venv.nu" ''
-        #!/usr/bin/env nu
+      mkprepare_venv = {writeScriptBin, python-with-packages, venvDir ? ".venv"}: (writeShellScriptBin "prepare_venv.sh" ''
         # Create virtualenv if doesn't exist
-        if not ("./${venvDir}" | path exists) {
-          print "creating venv..."
+        if test ! -d "./${venvDir}"; then
+          echo "creating venv..."
           virtualenv "./${venvDir}" --python="${python-with-packages}/bin/python3" --system-site-packages
-          $env.PYTHONHOME = "${python-with-packages}"  # This has to be set AFTER creating virtualenv but BEFORE installing requirements. That is because the --system-site-packages tells the virtualenv to use system packages when they are available (such as the tricky numpy package).
+          export PYTHONHOME="${python-with-packages}"  # This has to be set AFTER creating virtualenv but BEFORE installing requirements. That is because the --system-site-packages tells the virtualenv to use system packages when they are available (such as the tricky numpy package).
 
-          if (requirements.txt | path exists) {
-            print "installing dependencies... ('pip install -r requirements.txt')"
+          if test -f requirements.txt; then
+            echo "installing dependencies... ('pip install -r requirements.txt')"
             ${venvDir}/bin/pip install -r requirements.txt
-          }
-          if (setup.py | path exists) or (pyproject.toml | path exists) {
-            print "installing dependencies... ('pip install -e .')"
+          fi
+          if test -f setup.py || test -f pyproject.toml; then
+            echo "installing dependencies... ('pip install -e .')"
             ${venvDir}/bin/pip install -e .
-          }
-        } else {
-          print "venv already exists"
-        }
+          fi
+        fi
 
-        $env.PYTHONHOME = "${python-with-packages}"
-      '');
+        export PYTHONHOME="${python-with-packages}"
+      '';
     };
   };
 }
